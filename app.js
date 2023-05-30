f20d5a83b6d342593e7298ab08973cd0791b9d38
Abraham Medina <abrahammedina220@gmail.com>
2023-05-30 08:27:15 -0300

    proyecto final js

---
+++ app.js
@@ -0,0 +1,70 @@
+const btnCart = document.querySelector('.container-cart-icon');
+
+const containercartproducts = document.querySelector('.container-cart-products');
+
+btnCart.addEventListener('click',() => {
+    containercartproducts.classList.toggle
+    ('hidden-cart')
+});
+
+//******************* */
+
+const cartInfo = document.querySelector('.cart-product');
+const rowProduct = document.querySelector('.row-product');
+
+//lista de todos los contenedores de productos//
+
+const productsList = document.querySelector('.container-items')
+
+//variable de arreglo de productos//
+
+let allProducts = [];
+
+
+productsList.addEventListener('clik' , e => {
+
+    if(e.target.classList.contains('btn-add-cart')){
+        const product = e.target.parentElemnt
+
+        const infoProduct = {
+            quantity: 1,
+            title: product.querySelector('h2').textContent,
+            price: product.querySelector('p').textContent,
+        }
+
+        allProducts = [...allProducts, infoProduct]
+    }
+    
+    console.log(allProducts)
+})
+
+const showHTML = ()=> {
+
+
+    allProducts.forEach(product => {
+        const containerProduct = document.createElement('div')
+        containerProduct.classList.add('cart-product')
+
+        containerProduct.innerHTML = `<div class="info-cart-product">
+        <span class="cantidad-producto-carrito">${product.quantity}</span>
+        <p class="titulo-producto-carrito">${product.title}</p>
+        <span class="precio-producto-carrito">${product.price}</span>
+    </div>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="icon-close"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M6 18L18 6M6 6l12 12"
+        />
+    </svg> `;
+    })
+
+}
+
